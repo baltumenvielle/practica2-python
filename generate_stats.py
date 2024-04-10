f477757c6c0c150c97c5094434ba831b4b2b606a
baltazarmenvielle <baltazarmenvielle@gmail.com>
2024-04-10 12:51:56 -0300

    Se reemplazó la funcionalidad del generador de estadísticas por un zip(), lo que resulta más simple

--- generate_stats.py
+++ generate_stats.py
@@ -1,10 +1,6 @@
 def generate_stats(names, goals, goals_avoided, assists):
   names = names.split(',')    
 
-  stats = []
-
-  for i in range(len(names)):
-    player = (names[i], goals[i], goals_avoided[i], assists[i])
-    stats.append(player)
+  stats = tuple(zip(names, goals, goals_avoided, assists))
 
   return stats
