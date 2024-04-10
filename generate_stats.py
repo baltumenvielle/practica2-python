3f5c4172dbf337610690e219944c5e08839a6ffe
baltazarmenvielle <baltazarmenvielle@gmail.com>
2024-04-10 19:56:16 -0300

    Se arregló el problema del formato en los nombres

--- generate_stats.py
+++ generate_stats.py
@@ -2,7 +2,9 @@ def generate_stats(names, goals, goals_avoided, assists):
   for name in names:
     if name[0] == " " or name[0] == "\n":
       names = names.replace(name[0], '')
-  names = names.capitalize().split(',')    
+
+  names = names.split(',') 
+  names = [name.capitalize() for name in names]
   
 
   stats = tuple(zip(names, goals, goals_avoided, assists))
