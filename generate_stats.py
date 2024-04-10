372f3f5468704b7979ffc31bbc3dafdfc9315627
baltazarmenvielle <baltazarmenvielle@gmail.com>
2024-04-10 15:53:22 -0300

    Se arreglaron las inconsistencias en el formato de los nombres

--- generate_stats.py
+++ generate_stats.py
@@ -1,5 +1,9 @@
 def generate_stats(names, goals, goals_avoided, assists):
-  names = names.split(',')    
+  for name in names:
+    if name[0] == " " or name[0] == "\n":
+      names = names.replace(name[0], '')
+  names = names.capitalize().split(',')    
+  
 
   stats = tuple(zip(names, goals, goals_avoided, assists))
 
